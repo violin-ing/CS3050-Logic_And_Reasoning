95a3ffb2ff407c6c3551caf9807b2813423d08d7
violin-ing <eugenekhy@gmail.com>
2025-11-21 00:54:32 +0000

    updated build.sh and README

--- Practical/build.sh
+++ Practical/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-javac ./src/*.java
+# Compile Java files
+# javac ./src/*.java
+javac ./src/LRChecker.java
+
+# Create wrapper for LRChecker
+rm lrchecker
+touch lrchecker
+echo '#!/bin/sh' > lrchecker
+echo 'java -cp src LRChecker "$@"' >> lrchecker
+chmod +x lrchecker
