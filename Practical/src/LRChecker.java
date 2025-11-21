7a0bb0a48a1e443de331e56132ea098a28385cc7
violin-ing <eugenekhy@gmail.com>
2025-11-21 00:40:28 +0000

    poop

--- Practical/src/LRChecker.java
+++ Practical/src/LRChecker.java
@@ -1,26 +1,85 @@
-public class LRChecker {
-    public static void main(String[] args) {
-        bool test_mode, check_mode = false;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+
+public class LRChecker {
+    private static boolean test_mode = false;
+    private static boolean check_mode = false;
+
+
+    private static void args_validation(String args[]) {
         // Parse arguments
-        if (args.length != 3) {
-            System.err.println("error: usage: lrchecker [--check | --test] file.[nd | sq]");
-            exit();
+        if (args.length != 2) {
+            System.err.println("error: usage: lrchecker [--check | --test] [file.nd | file.sq]");
+            System.exit(1);
         }
 
-        if (args[1].equals("--check")) {
+        if (args[0].equals("--check")) {
             check_mode = true;
         } else if (args[1].equals("--test")) {
             test_mode = true;
         } else {
-            System.err.println("error: usage: lrchecker [--check | --test] file.[nd | sq]");
-            exit();
+            System.err.println("error: usage: lrchecker [--check | --test] [file.nd | file.sq]");
+            System.exit(1);
+        }
+    }
+
+    private static void file_validation(String file_name) {
+        // Check file existence
+        File file_obj = new File(file_name);
+        if (!file_obj.isFile()) {
+            System.err.printf("error: cannot access '%s': No such file or directory\n", file_name);
+            System.exit(1);
         }
 
-        String file_name = args[2];
+        // Check file extension
+        String[] file_name_parts = file_name.split("\\.");
+        int len = file_name_parts.length;
+        String file_ext = (len == 1) ? "" : file_name_parts[len - 1];
+        if (!(file_ext.equals("nd") || file_ext.equals("sq"))) {
+            System.err.printf("error: '%s': file extension not allowed\n", file_name);
+            System.exit(1);
+        }
+    }
 
-        // Checking file extension
-        String file_ext = 
+
+    private static String[] parse_file(String file_name) throws IOException{
+        List<String> file_lines = new ArrayList<String>();
+
+        BufferedReader buf = new BufferedReader(new FileReader(file_name));
+        String line;
+        while ((line = buf.readLine()) != null) {
+            file_lines.add(line);
+        }
+
+        return file_lines.toArray(new String[0]);
+    }
+
+
+    public static void main(String[] args) {
+        // Validate arguments
+        args_validation(args);
+
+        // Validate file
+        String file_name = args[1].trim();
+        file_validation(file_name);
+
+        // Parse file contents -> obtain array of lines
+        String[] file_lines = null;
+        try {
+            file_lines = parse_file(file_name);
+        } catch(IOException e) {
+            System.err.println();
+            System.exit(1);
+        } finally {
+            for (String x : file_lines) System.out.println(x);
+        }
     }
 }
 
